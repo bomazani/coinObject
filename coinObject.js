23f42d64e468b1dab7d6669ccceb21903a8e1ef3
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-14 11:36:31 -0400

    cleaned up css. working on adding a button and the ability for user to enter the number of tosses.

--- coinObject.js
+++ coinObject.js
@@ -6,53 +6,51 @@ var coin = {
     state: 0,
 
     flip: function () {
-        const coinTossResult = Math.floor(Math.random() * 2);
         // randomly set this.state to be either 0 or 1
+        const coinTossResult = Math.floor(Math.random() * 2);
         if (coinTossResult === 0) {
             this.state = coinTossResult;
         } else {
             this.state = coinTossResult;
         }
-       
     },
 
 
-    toString: function () {
+    aString: function () {
         // return “H” or “T” depending on whether this.state is 0 or 1
         if (this.state === 0) {
             return "Heads";
         } else {
             return "Tails";
         }
-
     },
 
 
     toHTML: function () {
-        var img = document.createElement("img");
         // set the properties of the image element to show either heads or tails
+        var img = document.createElement("img");
         if (this.state === 0) {
-
             img.src = "goYenFront.png";
         } else {
-            img.src = "goYenBack.png";;
+            img.src = "goYenBack.png";
         }
         return img;
     }
-
 }
-var tossResult = "Result:";
+
+
+var tossResult = "Coin toss results: ";
 for (let i = 0; i<20; i++) {
 coin.flip();
 
-var tossResultString = coin.toString();
-tossResult = tossResult + tossResultString + " ";
+
+var tossResultString = coin.aString();
+tossResult = tossResult + tossResultString + ", ";
 
 
 var tossResultImage = coin.toHTML();
 console.log(tossResultImage);
 document.getElementById("displayImage").appendChild(tossResultImage);
-
 }
 
 var resultString = document.createElement("div");
